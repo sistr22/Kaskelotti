c7cef0d6b0d63a10d0a871f02f3d94c81a7b1972
Damien Mabin <damien.mabin@supercell.com>
2020-02-25 07:47:51 +0200

    Update DB and check DB CRC to replace older DB on the client when shipping a new one.

--- app/src/main/java/com/sistr/kaskelotti/DbHelper.kt
+++ app/src/main/java/com/sistr/kaskelotti/DbHelper.kt
@@ -3,18 +3,56 @@ package com.sistr.kaskelotti
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
-import android.net.Uri
-import java.io.File
-import java.io.FileOutputStream
-import java.net.URI
+import android.util.Log
+import java.io.*
+import java.util.zip.CRC32
 
 
 class DbHelper(val context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
+    private val _tag = "DbHelper"
     private val DB_NAME = "sqlite3.db"
+
+    @Throws(IOException::class)
+    private fun getCRC(file: File): Long {
+        val fis = FileInputStream(file)
+        val data = ByteArray(file.length().toInt())
+        fis.read(data)
+        fis.close()
+        val crc32 = CRC32()
+        crc32.update(data)
+        return crc32.value
+    }
 
     init {
         if (!File(context.getDatabasePath(DB_NAME).path).exists()) {
             copyDatabase()
+        } else {
+            val crcCopiedDb = getCRC(File(context.getDatabasePath(DB_NAME).path))
+            Log.d(_tag, "CRC copied DB: $crcCopiedDb")
+
+            val inputStream = context.assets.open(DB_NAME)
+            val bufferedReader = BufferedInputStream(inputStream)
+            val buff = ByteArray(128)
+            var sizeRead: Int
+            val crc32 = CRC32()
+            do {
+                sizeRead = bufferedReader.read(buff)
+                if(sizeRead == -1)
+                    continue
+                if(sizeRead == buff.size)
+                    crc32.update(buff)
+                else {
+                    crc32.update(buff.copyOfRange(0, sizeRead))
+                }
+
+            } while (sizeRead > 0)
+
+            Log.d(_tag, "CRC assets DB: ${crc32.value}")
+
+            if(crc32.value != crcCopiedDb) {
+                copyDatabase()
+            }
+
         }
     }
 
